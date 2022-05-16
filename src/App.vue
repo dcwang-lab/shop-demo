03610390ddaaa7992faf6631af9c826370e67f3b
dcwang <dcwang@zkbsn.com>
2022-05-16 11:10:20 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import 'assets/css/base';
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+.tab-bar-item {
+  flex: 1;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  height: 49px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      // 路径别名
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'network': '@/network',
+        'utils': '@/utils',
+        'views': '@/views'
+      }
+    }
+  }
 })
