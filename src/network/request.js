49d72596264c167d500833efd3fe94cf94ccbb74
dcwang <dcwang@zkbsn.com>
2022-06-07 10:08:35 +0800

    提交

--- src/network/request.js
+++ src/network/request.js
@@ -5,7 +5,8 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 // 创建axios实例
 const service = axios.create({
   // 请求url公共部分
-  baseURL: 'http://127.0.0.1:8070/shop-commodity',
+  baseURL: 'http://127.0.0.1/shop-commodity',
+  // baseURL: 'http://127.0.0.1:8070',
   // 超时时间
   timeout: 10000
 });
@@ -19,8 +20,10 @@ service.interceptors.request.use(config => {
 });
 
 // response拦截器
-service.interceptors.response.use(res => {
-  return res.data;
+service.interceptors.response.use(response => {
+  console.log(111);
+  console.log(response);
+  return response.data;
 }, error => {
   console.log(error);
 });
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -14,20 +14,8 @@
   </div>
   <div class="sort">
     <ul>
-      <li>热门推荐</li>
-      <li>手机数码</li>
-      <li>家用电器</li>
-      <li>电脑办公</li>
-      <li>玩具乐器</li>
-      <li>家具厨具</li>
-      <li>家居家装</li>
-      <li>男装</li>
-      <li>男鞋</li>
-      <li>女装</li>
-      <li>女鞋</li>
-      <li>美妆护肤</li>
-      <li>医药保健</li>
-      <li>酒水饮料</li>
+      <li v-for="item in listCategorys" :key="item.id" @click="liClick(item.id)" :class="{active: item.id==id}">
+        {{item.name}}</li>
     </ul>
   </div>
 </template>
@@ -38,21 +26,64 @@ export default {
   name: 'Category',
   components: {
     NavBar
+  },
+  data() {
+    return {
+      id: 0,
+      listCategorys: [
+        { id: 1, name: '热门推荐' },
+        { id: 2, name: '手机数码' },
+        { id: 3, name: '家用电器' },
+        { id: 4, name: '电脑办公' },
+        { id: 5, name: '玩具乐器' },
+        { id: 6, name: '家具厨具' },
+        { id: 7, name: '家居家装' },
+        { id: 8, name: '男装' },
+        { id: 9, name: '男鞋' },
+        { id: 10, name: '女装' },
+        { id: 11, name: '女鞋' },
+        { id: 12, name: '美妆护肤' },
+        { id: 13, name: '医药保健' },
+        { id: 14, name: '酒水饮料' },
+        { id: 15, name: '户外运动' },
+        { id: 16, name: '二手商品' },
+        { id: 17, name: '生活旅行' }
+      ]
+    }
+  },
+  methods: {
+    liClick(id) {
+      this.id = id;
+    }
   }
 }
 </script>
 
 <style scoped>
+.active {
+  color: #cd5c5c;
+  background-color: #fff;
+  border: 1px solid #f8f8f8;
+}
 .sort {
   position: relative;
   z-index: 1;
   width: 25%;
   text-align: center;
   background-color: #f8f8f8;
+  height: 540px;
+  overflow-y: auto;
+}
+/* 去掉滚动条 */
+.sort::-webkit-scrollbar {
+  display: none;
 }
 .sort li {
   height: 50px;
   line-height: 50px;
+  white-space: nowrap; /*不让文字内容换行*/
+  overflow: hidden; /*文字溢出的部分隐藏起来*/
+  text-overflow: ellipsis; /*用...替代溢出的部分*/
 }
 .home-nav {
   background-color: #fff;
